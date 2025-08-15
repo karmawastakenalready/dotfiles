fe717c0fc4614d057d3e0b83b6f43ab8bc91b386
karmawastakenalready <175164999+karmawastakenalready@users.noreply.github.com>
2025-08-15 15:22:50 -0700

    Add yazi to powershell profile / glazewm default config

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
 Import-Module 'gsudoModule'
 oh-my-posh init pwsh --config 'https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/easy-term.omp.json'| Invoke-Expression
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
+
